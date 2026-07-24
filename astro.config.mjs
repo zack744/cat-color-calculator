223716aaeb68dc5265a93cef1f13f5b6671a1b7b
zackhe <zackhe774@gmail.com>
2026-07-25 02:04:33 +0800

    fix: set site canonical host to www

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://catcolorcalculator.icu',
+  site: 'https://www.catcolorcalculator.icu',
   trailingSlash: 'always',
   integrations: [
     tailwind({
